939abfaf2b44137f7b8961a9d236909b43a7ed30
Ruleer <41296448+Ruleer@users.noreply.github.com>
2022-02-25 15:04:45 +0800

    feat: add stastic card

---
+++ src/components/StasticCard.vue
@@ -0,0 +1,157 @@
+<!-- StasticCard -->
+<template>
+    <div class='StasticCard'>
+        <div class="left">
+            <div class="label">
+                <span>{{ label }}</span>
+                <el-button class="ruleBtn" type="text">{{ rule }}</el-button>
+            </div>
+            <div class="data">
+                <span>{{ data }}</span>
+            </div>
+            <div class="compare">
+                <div class="iconContainer"  v-if="type!=='totalSize'">
+                    <img class="icon" src="../assets/img/rise.svg" />
+                </div>
+                <div class="rate"  v-if="type!=='totalSize'">
+                    <span>{{ rate }}</span>
+                </div>
+                <div class="description">
+                    <span>{{ description }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="imgContainer" :style="{ backgroundColor: imgList[type].color }">
+                <img class="img" :src="imgList[type].src" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+const imgList = {
+    totalSize: {
+        src: '/user.svg',
+        color: '#dc7c38'
+    },
+    failSize: {
+        src: '/failure.svg',
+        color: '#f5444f'
+    },
+    excellentRate: {
+        src: '/medal.svg',
+        color: '#27b488'
+    },
+    passRate: {
+        src: '/graph.svg',
+        color: '#0f8fd1'
+    }
+}
+
+export default {
+    name: 'StasticCard',
+    props: [
+        'label',
+        'data',
+        'rate',
+        'type',
+        'description',
+        'rule'
+    ],
+    components: {},
+    data () {
+        return {
+            imgList
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang='scss' scoped>
+.StasticCard {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    padding: 16px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: #555555 1px 1px 20px;
+}
+
+.left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 85%;
+    height: 100%;
+
+    .label {
+        display: flex;
+        align-items: center;
+        color: #6b6b6b;
+        font-size: 16px;
+        line-height: 16px;
+
+        .ruleBtn {
+            margin-left: 4px;
+            font-size: 12px;
+            line-height: 12px;
+            text-decoration: underline;
+        }
+    }
+
+    .data {
+        margin-left: 4%;
+        color: #555555;
+        font-size: 24px;
+    }
+
+    .compare {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+
+        .icon {
+            width: 20px;
+        }
+
+        .rate {
+            margin-left: 2px;
+            color: #67c23a;
+        }
+
+        .description {
+            margin-left: 12px;
+            color: #6b6b6b;
+            font-size: 12px;
+        }
+    }
+}
+
+.right {
+    display: flex;
+    justify-content: flex-end;
+    width: auto;
+
+    .imgContainer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        top: 8px;
+        height: 48px;
+        width: 48px;
+        background-color: #0f83d1;
+        border-radius: 50%;
+
+        .img {
+            width: 20px;
+        }
+    }
+}
+</style>
--- src/pages/MainPage/views/GradeScore.vue
+++ src/pages/MainPage/views/GradeScore.vue
@@ -1,16 +1,104 @@
 <!-- 年级成绩 -->
 <template>
-    <div class='GradeScore'>GradeScore</div>
+    <div class='GradeScore'>
+        <div class="filter">
+            <el-form :inline="true" :model="filterForm" class="filter-form">
+                <el-form-item label="专业:">
+                    <el-select v-model="filterForm.major" placeholder="请选择专业">
+                        <el-option label="专业1" value="major1"></el-option>
+                        <el-option label="专业2" value="major2"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="年级:">
+                    <el-select v-model="filterForm.grade" placeholder="请选择年级">
+                        <el-option label="2000级" value="2000"></el-option>
+                        <el-option label="2001级" value="2001"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="学期:">
+                    <el-select v-model="filterForm.term" placeholder="请选择学期">
+                        <el-option label="200001" value="200001"></el-option>
+                        <el-option label="200002" value="200002"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item>
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="charts">
+            <div class="row-1">
+                <div class="colum-1">
+                    <div class="stastic" v-for="stastic in stasticList" :key="stastic.type">
+                        <StasticCard
+                            :label="stastic.label"
+                            :data="stastic.data"
+                            :rate="stastic.rate"
+                            :type="stastic.type"
+                            :description="stastic.description"
+                            :rule="stastic.rule"
+                        ></StasticCard>
+                    </div>
+                </div>
+                <div class="column-2">
+                </div>
+            </div>
+            <div class="row-2">
+                <div class="colum-1">
+                </div>
+                <div class="column-2">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 
+import StasticCard from '../../../components/StasticCard.vue';
+
 export default {
     name: 'GradeScore',
-    components: {},
+    components: {
+        StasticCard
+    },
     data () {
         return {
-
+            filterForm: {
+                major: '',
+                grade: '',
+                term: ''
+            },
+            stasticList: [
+                {
+                    label: '总人数',
+                    data: '999',
+                    type: 'totalSize'
+                },
+                {
+                    label: '挂科人数',
+                    data: '23',
+                    type: 'failSize',
+                    rate: '3.48%',
+                    description: '自上学期以来'
+                },
+                {
+                    label: '优秀率',
+                    data: '52.12%',
+                    type: 'excellentRate',
+                    rule: '优秀率规则',
+                    rate: '3.48%',
+                    description: '自上学期以来'
+                },
+                {
+                    label: '及格率 (通过所有科目)',
+                    data: '79.65%',
+                    type: 'passRate',
+                    rule: '及格分数线',
+                    rate: '3.48%',
+                    description: '自上学期以来'
+                },
+            ]
         }
     },
     methods: {
@@ -19,5 +107,53 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
-//@import url();
+
+.GradeScore {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+}
+
+.filter {
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+    box-sizing: border-box;
+    width: 100%;
+    height: 56px;
+    background-color: #fff;
+
+    .filter-form {
+        height: 32px;
+    }
+}
+
+.charts {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    padding: 8px;
+    box-sizing: border-box;
+
+    .row-1 {
+        display: flex;
+        width: 100%;
+
+        .colum-1 {
+            display: flex;
+            flex-wrap: wrap;
+            width: 55%;
+
+            .stastic {
+                display: flex;
+                width: 50%;
+                height: 160px;
+                padding: 8px;
+                box-sizing: border-box;
+            }
+        }
+    }
+}
+
 </style>
