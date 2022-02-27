9f4d44a7daf320e3472f6eccbd98276aff8732bb
Ruleer <41296448+Ruleer@users.noreply.github.com>
2022-02-27 19:46:46 +0800

    feat: finishi change password

--- src/pages/MainPage/views/AccountManage.vue
+++ src/pages/MainPage/views/AccountManage.vue
@@ -2,7 +2,55 @@
 <template>
     <div class='AccountManage'>
         <div class="contentCard">
-            <div class="changeForm"></div>
+            <span class="cardTitle">修改密码</span>
+            <div class="changeForm">
+                <el-form
+                    :model="changeForm"
+                    style="max-width: 460px"
+                    label-position="Right"
+                    label-width="auto"
+                >
+                    <el-form-item
+                        label="旧密码"
+                        :rules="[
+                            {
+                                required: true,
+                                message: '请输入旧密码',
+                                trigger: 'blur',
+                            }
+                        ]"
+                    >
+                        <el-input v-model="changeForm.oldPass" placeholder="6 - 16位密码，区分大小写"></el-input>
+                    </el-form-item>
+                    <el-form-item
+                        label="新密码"
+                        :rules="[
+                            {
+                                required: true,
+                                message: '请输入新密码',
+                                trigger: 'blur',
+                            }
+                        ]"
+                    >
+                        <el-input v-model="changeForm.newPass" placeholder="6 - 16位密码，区分大小写"></el-input>
+                    </el-form-item>
+                    <el-form-item
+                        label="确认新密码"
+                        :rules="[
+                            {
+                                required: true,
+                                message: '请再次输入新密码',
+                                trigger: 'blur',
+                            }
+                        ]"
+                    >
+                        <el-input v-model="changeForm.confirmPass" placeholder="6 - 16位密码，区分大小写"></el-input>
+                    </el-form-item>
+                    <el-form-item class="btnContainer">
+                        <el-button type="primary" @click="confirmChange">确认修改</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
         </div>
     </div>
 </template>
@@ -14,14 +62,36 @@ export default {
     components: {},
     data () {
         return {
-
+            changeForm: {
+                oldPadd: '',
+                newPass: '',
+                confirmPass: ''
+            }
         }
     },
     methods: {
+        confirmChange() {
 
+        }
     }
 }
 </script>
 <style lang='scss' scoped>
-//@import url();
+.AccountManage {
+    width: 100%;
+    justify-content: center;
+    padding: 32px;
+    box-sizing: border-box;
+}
+
+.contentCard {
+    display: flex;
+    flex-direction: column;
+
+    .changeForm {
+        display: flex;
+        justify-content: center;
+        margin-top: 16px;
+    }
+}
 </style>
