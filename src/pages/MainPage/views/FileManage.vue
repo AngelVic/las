8bf5a4c125a611771a601241e8e273c959ed03c8
Ruleer <41296448+Ruleer@users.noreply.github.com>
2022-05-17 21:56:17 +0800

    fix: 禁用已删除文件的删除操作

--- src/pages/MainPage/views/FileManage.vue
+++ src/pages/MainPage/views/FileManage.vue
@@ -109,7 +109,7 @@
                             </el-table-column>
                             <el-table-column label="操作">
                                 <template #default="scope">
-                                    <el-button type="text" @click="deleteFile(scope.row.id)">删除文件</el-button>
+                                    <el-button type="text" @click="deleteFile(scope.row.id)" :disabled="scope.row.state=='2'">删除文件</el-button>
                                 </template>
                             </el-table-column>
                         </el-table>
